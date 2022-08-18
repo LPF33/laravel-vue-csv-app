bb8bff97b642813ff447f681d6324749582d738f
lfeigl <larspefe@gmail.com>
2022-08-18 22:17:45 +0200

    deleted validation for data in request, if string is empty

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -109,13 +109,7 @@ class Controller extends BaseController
 
         $newRow = collect($collection['data'])->map(fn ($value) => trim(strip_tags($value)));
 
-        if ($newRow->filter(function ($value, $key) {
-            if (preg_match('/Ärmel|Bein|Kragen|Herstellung|Taschenart|Grammatur|Ursprungsland|Bildname/', $key)) {
-                return false;
-            }
-
-            return true;
-        })->contains(fn ($value) => $value === '') || $rowIndex === false) {
+        if ($rowIndex === false) {
             return response()->json(['error' => 'Incorrect input']);
         }
 
@@ -160,16 +154,6 @@ class Controller extends BaseController
         $collection = $request->collect();
 
         $newRow = collect($collection['data'])->map(fn ($value) => trim(strip_tags($value)));
-
-        if ($newRow->filter(function ($value, $key) {
-            if (preg_match('/Ärmel|Bein|Kragen|Herstellung|Taschenart|Grammatur|Ursprungsland|Bildname/', $key)) {
-                return false;
-            }
-
-            return true;
-        })->contains(fn ($value) => $value === '')) {
-            return response()->json(['error' => 'Incorrect input', 'data' => $newRow]);
-        }
 
         $fileResource = fopen(self::FILE_PATH, 'a');
 
